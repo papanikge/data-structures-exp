37d0c66045ad97d16ebae7e7f765d526808c9e07
George Papanikolaou <g3orge.app@gmail.com>
2013-04-18 16:35:33 +0300

    create and move some stuff to a header file

--- base.c
+++ base.c
@@ -7,70 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/* maximum sizes (included pointed strings) */
-#define MAXAUTHOR 100
-#define MAXTITLE 256
-#define MAXPUBL  40
-#define MAXENTRY (MAXTITLE + MAXPUBL + sizeof(Book))
-
-/* for the individual ids of book structs */
-long idSum = 0;
-
-/* author type for names */
-typedef struct {
-	char *first;
-	char *last;
-} Author;
-
-/* a single unit type */
-/* more than one author possible */
-typedef struct {
-	long   id;
-	char   *title;
-	char   *publisher;
-	short  yearPublished;
-	int    numberOfAuthors;
-	Author *authors;
-} Book;
-
-/* the wrapper around the dynamic array of the managed books */
-typedef struct {
-	Book  *arr;
-	long  numberOfBooks;
-} Data;
+#include "books.h"
 
 /* global main in-memory dynamic database */
 Data db;
-
-/* panic function for fatal errors */
-inline void fatal(char *message)
-{
-	fprintf(stderr, "\a*** Fatal error: %s\n", message);
-	exit(EXIT_FAILURE);
-}
-
-/* a fail-aware malloc function */
-void* smalloc(const unsigned int size)
-{
-	void *ptr;
-	ptr = malloc(size);
-	if(!ptr)
-		fatal("on memory allocation");
-	return ptr;
-}
-
-/* get a valid option from user */
-int get_option(void)
-{
-	int in;
-	do {
-		printf("\nOption? [1..9] ");
-		in = getchar();
-	} while (in < 48 || in > 57);
-	/* damn you ascii */
-	return (in - 48);
-}
+/* for the individual ids of book structs */
+long idSum = 0;
 
 /* create and return a book struct */
 Book* create_book(char* name, short year, char* pub)
---
+++ books.h
@@ -0,0 +1,61 @@
+/**
+ * Books data structures CEID project header file
+ */
+
+/* maximum sizes (included pointed strings) */
+#define MAXAUTHOR 100
+#define MAXTITLE 256
+#define MAXPUBL  40
+#define MAXENTRY (MAXTITLE + MAXPUBL + sizeof(Book))
+
+/* author type for names */
+typedef struct {
+	char *first;
+	char *last;
+} Author;
+
+/* a single unit type */
+/* more than one author possible */
+typedef struct {
+	long   id;
+	char   *title;
+	char   *publisher;
+	short  yearPublished;
+	int    numberOfAuthors;
+	Author *authors;
+} Book;
+
+/* the wrapper around the dynamic array of the managed books */
+typedef struct {
+	Book  *arr;
+	long  numberOfBooks;
+} Data;
+
+/* panic function for fatal errors */
+inline void fatal(char *message)
+{
+	fprintf(stderr, "\a*** Fatal error: %s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+/* a fail-aware malloc function */
+void* smalloc(const unsigned int size)
+{
+	void *ptr;
+	ptr = malloc(size);
+	if(!ptr)
+		fatal("on memory allocation");
+	return ptr;
+}
+
+/* get a valid option from user */
+int get_option(void)
+{
+	int in;
+	do {
+		printf("\nOption? [1..9] ");
+		in = getchar();
+	} while (in < 48 || in > 57);
+	/* damn you ascii */
+	return (in - 48);
+}
